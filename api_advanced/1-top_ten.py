64fa35d28e14cab9c6cc3eef3c2d581fd6c5e22f
glenmiracle18 <m.bonyu@alustudent.com>
2023-11-10 16:29:42 +0200

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,16 +7,23 @@ api
 import requests
 import sys
 
-"""
-    main function for the subcriber count
-"""
+
 def top_ten(subreddit):
-    #  main moadule
-    api_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    try:
-        response = requests.get(api_url, allow_redirects=False)
-        data = response.json()
-        return data['data']['children']['data'][4]['title']
-    except requests.exceptions.RequestException as e:
-        print(e)
-        return 0
+    """
+    Sends a query to the reddit api to get the hot ten titles
+    for a particular subreddit
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return (0)
+    endpoint = 'https://www.reddit.com'
+    headers = {'user-agent': 'Testapi/1.0 by glenmiracle18'}
+    params = {'limit': 10}
+    info = requests.get('{}/r/{}/hot.json'.format(
+        endpoint,
+        subreddit), headers=headers, params=params, allow_redirects=False)
+    if info.status_code == 200:
+        data_info = info.json()
+        for post in data_info.get('data').get('children'):
+            print(post.get('data').get('title'))
+    else:
+        return (0)
